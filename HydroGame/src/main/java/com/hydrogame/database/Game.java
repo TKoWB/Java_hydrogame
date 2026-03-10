f901bf540bb91b29932b3a3cf4f191e60ed528c0
KanZick <nguyenbaolam01112006@gmail.com>
2026-03-10 13:41:00 +0700

    thay doi 1 dau cach

--- HydroGame/src/main/java/com/hydrogame/database/Game.java
+++ HydroGame/src/main/java/com/hydrogame/database/Game.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
 package com.hydrogame.database;
-import javax.persistence.*;
-import java.time.LocalDate;
 import java.math.BigDecimal;
-import java.util.Set;
+import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "game")
@@ -85,7 +95,7 @@ public class Game {
     public String getImgUrl() { return imgUrl; }
     public void setImgUrl(String imgUrl) { this.imgUrl = imgUrl; }
 
-    //tostring
+    //to string
     @Override
     public String toString() {
         return "Game{" +
